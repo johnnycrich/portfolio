e0d6b5f0430589569bc8d1ab9a4a44e7679632fc
Johnny Richardson <johnny.c.rich@gmail.com>
2020-10-19 18:24:08 -0400

    Content updates.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,12 +7,19 @@ export const mapped =
 _.mapValues(all, (file) => {
 	return file.html;
 });
-console.log(all)
 </script>
 <style>
- img {
-	 filter: grayscale(100%);
- }
+	#content {
+		overflow-y: scroll;
+		position: absolute; 
+		top: 0; 
+		left: 1%; 
+		right: 0; 
+		bottom: 0;
+	}
+	#about {
+		padding-top: 5%;
+	}
 </style>
 
 <svelte:head>
@@ -21,17 +28,23 @@ console.log(all)
 
 <Header />
 
-<div class="frow row-start">
-	<div class="col-md-1-3">
-		<h3>About</h3>
-		
-		{@html mapped['0']}
-	</div>
-</div>
+<section id="content">
 
-<div class="frow row-start">
-	<div class="col-md-1-2">
-<h3>Highlights</h3>
-{@html mapped['1']}
-</div>
-</div>
+	<div id="about" class="frow row-start">
+		<div class="col-md-1-3">
+			<h3>About</h3>
+			
+			{@html mapped['0']}
+		</div>
+	</div>
+
+	<div class="frow row-start">
+		<div class="col-md-1-2">
+			<h3>Highlights</h3>
+			<div id="highlights">
+				{@html mapped['1']}
+			</div>
+		</div>
+	</div>
+
+</section>
